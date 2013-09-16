fcc148ff6575bde3d069e2e04aa7165054d81f1b
Brian Picciano <bgp2396@bellsouth.net>
2013-09-15 21:53:59 -0400

    got rid of wrapped, we can use concated instead

--- src/chdl/alpha/expr.clj
+++ src/chdl/alpha/expr.clj
@@ -33,15 +33,6 @@
 
 (defn tabd [item] (prepended item "    "))
 
-(proto/defalpha-item wrapped
-  "Given an alpha-item, represents it but with the given chars/strings on either
-  side"
-  [ch1 item ch2]
-    (to-str [_]
-      (str ch1 (proto/to-str item) ch2)))
-
-(defn parend [item] (wrapped \( item \)))
-
 (proto/defalpha-item concated
   "Given one or more alpha items, represents their forms joined with nothing
   in-between"
@@ -49,23 +40,38 @@
     (to-str [_]
       (apply str (map proto/to-str items))))
 
+(proto/to-str (concated [(literal/raw :a) (literal/raw :b)]))
+
+(defn parend [item]
+  (concated [
+    (literal/raw \()
+    item
+    (literal/raw \)) ]))
+
 (comment
   (def s (space-sep [ (literal/raw :one) (literal/raw :two) (literal/raw :three)]))
   (proto/to-str s)
+
   (def c (comma-sep [ (literal/raw :one) (literal/raw :two) (literal/raw :three)]))
   (proto/to-str c)
+
   (def sc (semicolond c))
   (proto/to-str sc)
+
   (def cc (commad s))
   (proto/to-str cc)
+
   (def nc (newlined c))
   (proto/to-str nc)
+
   (def tc (tabd s))
   (proto/to-str tc)
-  (def pc (parend c))
-  (proto/to-str pc)
+
   (def tc (concated [s c (semicolond c)]))
   (proto/to-str tc)
+
+  (def pc (parend c))
+  (proto/to-str pc)
 
   (proto/to-str (tabd (newlined (semicolond s))))
 )
