e73c97f3ed769790ae560bd3108518908f79f62c
Marco Munizaga <marco.munizaga@grooveshark.com>
2013-09-16 23:18:37 -0400

    reworked defalpha-item macro

--- src/chdl/alpha/proto.clj
+++ src/chdl/alpha/proto.clj
@@ -4,8 +4,17 @@
   (to-str [x]))
 
 (defmacro defalpha-item [item-name doc args & body]
-  (let [item-name-t   (symbol (str (name item-name) "-t"))
-        item-name-t-dot (symbol (str (name item-name) "-t."))]
+  (let [item-type (gensym item-name)]
     `(do
-      (deftype ~item-name-t ~args alpha-item ~@body)
-      (defn ~item-name ~doc ~args ~(cons item-name-t-dot args)))))
+      (deftype ~item-type ~args alpha-item ~@body)
+      (defn ~item-name ~doc ~args (new ~item-type ~@args)))))
+
+(comment 
+  ;; Demonstrate the macro's functionality
+  (macroexpand  
+    '(defalpha-item numeric
+      "Given a base and a number in that base as a string, represents that number"
+      [base n]
+        (to-str [_] (str base "#" n "#"))))
+  )
+
