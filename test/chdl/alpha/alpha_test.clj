db8710a8da980785acdd38cb894362b51a171408
Marco Munizaga <marco.munizaga@grooveshark.com>
2013-09-16 23:18:56 -0400

    Test all the things! :D

---
+++ test/chdl/alpha/alpha_test.clj
@@ -0,0 +1,37 @@
+(ns chdl.alpha.alpha-test
+  (:require [clojure.test :refer :all]
+            [midje.sweet :refer :all]
+            [chdl.alpha.proto :as proto]
+            [chdl.alpha.expr :refer :all]
+            [chdl.alpha.literal :refer :all]))
+
+(facts "About the alpha layer"
+  (fact "Checking the literals"
+    (proto/to-str (raw :ohai))                      => "ohai"
+    (proto/to-str (numeric 10 23))                  => "10#23#"
+    (proto/to-str (num2 "0010"))                    => "2#0010#"
+    (proto/to-str (string "he said \"she said\""))  => "\"he said \"\"she said\"\"\""
+    (proto/to-str (bit-string :binary "001"))       => "B\"001\""
+    (proto/to-str (bit-string :octal "755"))        => "O\"755\""
+    (proto/to-str (bit-string :hex "ABC123"))       => "X\"ABC123\""
+    (proto/to-str (bool true))                      => "TRUE"
+    (proto/to-str (bool false))                     => "FALSE")
+
+  (fact "Testing expressions!"
+    (proto/to-str (concated (raw :a) (raw :b))) => "ab"
+    (let [s  (space-sepd (raw :one) (raw :two) (raw :three))
+          c  (comma-sepd (raw :one) (raw :two) (raw :three))
+          sc (semicolond c)
+          cc (commad s)
+          nc (newlined c)
+          tc (tabd s)
+          pc (parend c)
+          combo (tabd (newlined (semicolond s)))]
+      (proto/to-str s)     => "one two three"
+      (proto/to-str c)     => "one, two, three"
+      (proto/to-str sc)    => "one, two, three;"
+      (proto/to-str cc)    => "one two three,"
+      (proto/to-str nc)    => "one, two, three\n"
+      (proto/to-str tc)    => "    one two three"
+      (proto/to-str pc)    => "(one, two, three)"
+      (proto/to-str combo) => "    one two three;\n"))) 
--- test/chdl/core_test.clj
+++ test/chdl/core_test.clj
@@ -1,7 +1,9 @@
 (ns chdl.core-test
+  (:use midje.sweet)
   (:require [clojure.test :refer :all]
             [chdl.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(facts "Test if this is awesome"
+  (fact "This is awesome"
+    (= :awesome :awesome) => true))
+
