598495bf4eea1248420ad535e846821be686199e
Brian Picciano <bgp2396@bellsouth.net>
2013-10-07 16:05:50 -0400

    got design units done

---
+++ src/chdl/beta/design.clj
@@ -0,0 +1,61 @@
+(ns chdl.beta.design
+  (:require [chdl.alpha.literal :as lit]
+            [chdl.alpha.expr :as expr]
+            [chdl.alpha.proto :as proto]
+            [chdl.beta.comp :as comp]
+            [chdl.beta.math :as math]))
+
+(defn- design-unit
+  "Represents a general design unit of the form:
+  THING name IS stuff BEGIN more stuff END THING name"
+  [top-typ bottom-typ pre-beg post-beg]
+  (expr/newlined (expr/newlined
+    (expr/semicolond
+      (expr/concated
+        (expr/newlined
+          (expr/space-sepd
+            top-typ
+            (lit/raw :IS)))
+        (apply expr/concated
+          (map #(expr/tabd (expr/newlined (expr/semicolond %1))) pre-beg))
+        (expr/newlined (lit/raw :BEGIN))
+        (apply expr/concated
+          (map #(expr/tabd (expr/newlined (expr/semicolond %1))) post-beg))
+        (expr/space-sepd
+          (lit/raw :END)
+          bottom-typ))))))
+
+(defn entity
+  "Represents an entity with the given ports. Entities are simply an interface
+  declaration, their implementation is in the architecture"
+  [name ports]
+  (let [name-decl (expr/space-sepd (lit/raw :ENTITY) (lit/raw name))]
+    (design-unit name-decl name-decl [ports] [])))
+
+(defn architecture
+  [of name pre-beg post-beg]
+  (design-unit
+    (expr/space-sepd
+      (lit/raw :ARCHITECTURE)
+      (lit/raw name)
+      (lit/raw :OF)
+      (lit/raw of))
+    (expr/space-sepd
+      (lit/raw :ARCHITECTURE)
+      (lit/raw name))
+    pre-beg
+    post-beg))
+
+(comment
+  (println (proto/to-str
+    (entity :wat (comp/port
+      :inSig :in :BIT
+      :outSig :out :BIT))))
+
+  (println (proto/to-str
+    (architecture :wut :wat
+      [ (comp/signal :tmpSig1 :BIT (lit/bit 0))
+        (comp/signal :tmpSig2 :INTEGER) ]
+      [ (comp/assign :tmpSig1 (math/not (lit/raw :inSig)))
+        (comp/assign :outSig (lit/raw :tmpSig1)) ])))
+)
