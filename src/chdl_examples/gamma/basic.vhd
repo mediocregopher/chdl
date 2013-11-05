a4601b45516b55b57f3641ab67048282459321b4
Brian Picciano <bgp2396@bellsouth.net>
2013-11-05 16:56:16 -0500

    update gamma example

--- src/chdl_examples/gamma/basic.vhd
+++ src/chdl_examples/gamma/basic.vhd
@@ -28,13 +28,53 @@ ARCHITECTURE ARCH OF main IS
 BEGIN
     tmp(0) <= '0';
     cout <= tmp(8);
-    G__7212 : ENTITY fullAdder(ARCH) PORT MAP(a => a(0), b => b(0), cin => tmp(0), s => s(0), cout => tmp(1));
-    G__7213 : ENTITY fullAdder(ARCH) PORT MAP(a => a(1), b => b(1), cin => tmp(1), s => s(1), cout => tmp(2));
-    G__7214 : ENTITY fullAdder(ARCH) PORT MAP(a => a(2), b => b(2), cin => tmp(2), s => s(2), cout => tmp(3));
-    G__7215 : ENTITY fullAdder(ARCH) PORT MAP(a => a(3), b => b(3), cin => tmp(3), s => s(3), cout => tmp(4));
-    G__7216 : ENTITY fullAdder(ARCH) PORT MAP(a => a(4), b => b(4), cin => tmp(4), s => s(4), cout => tmp(5));
-    G__7217 : ENTITY fullAdder(ARCH) PORT MAP(a => a(5), b => b(5), cin => tmp(5), s => s(5), cout => tmp(6));
-    G__7218 : ENTITY fullAdder(ARCH) PORT MAP(a => a(6), b => b(6), cin => tmp(6), s => s(6), cout => tmp(7));
-    G__7219 : ENTITY fullAdder(ARCH) PORT MAP(a => a(7), b => b(7), cin => tmp(7), s => s(7), cout => tmp(8));
+    G__7999 : ENTITY fullAdder(ARCH) PORT MAP(a => a(0)
+    , b => b(0)
+    , cin => tmp(0)
+    , s => s(0)
+    , cout => tmp(1)
+    );
+    G__8000 : ENTITY fullAdder(ARCH) PORT MAP(a => a(1)
+    , b => b(1)
+    , cin => tmp(1)
+    , s => s(1)
+    , cout => tmp(2)
+    );
+    G__8001 : ENTITY fullAdder(ARCH) PORT MAP(a => a(2)
+    , b => b(2)
+    , cin => tmp(2)
+    , s => s(2)
+    , cout => tmp(3)
+    );
+    G__8002 : ENTITY fullAdder(ARCH) PORT MAP(a => a(3)
+    , b => b(3)
+    , cin => tmp(3)
+    , s => s(3)
+    , cout => tmp(4)
+    );
+    G__8003 : ENTITY fullAdder(ARCH) PORT MAP(a => a(4)
+    , b => b(4)
+    , cin => tmp(4)
+    , s => s(4)
+    , cout => tmp(5)
+    );
+    G__8004 : ENTITY fullAdder(ARCH) PORT MAP(a => a(5)
+    , b => b(5)
+    , cin => tmp(5)
+    , s => s(5)
+    , cout => tmp(6)
+    );
+    G__8005 : ENTITY fullAdder(ARCH) PORT MAP(a => a(6)
+    , b => b(6)
+    , cin => tmp(6)
+    , s => s(6)
+    , cout => tmp(7)
+    );
+    G__8006 : ENTITY fullAdder(ARCH) PORT MAP(a => a(7)
+    , b => b(7)
+    , cin => tmp(7)
+    , s => s(7)
+    , cout => tmp(8)
+    );
 END ARCHITECTURE main;
 
