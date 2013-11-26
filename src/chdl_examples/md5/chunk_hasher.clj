cd834e3b176367d9f7eb406f354203550c5d8dbd
Brian Picciano <bgp2396@bellsouth.net>
2013-11-25 19:10:38 -0500

    formatting in chunk_hasher

--- src/chdl_examples/md5/chunk_hasher.clj
+++ src/chdl_examples/md5/chunk_hasher.clj
@@ -6,30 +6,19 @@
            [chdl.gamma.types :as t :refer :all]
            [chdl.gamma.chip :as chip :refer :all]
            [chdl.beta.control :as control]
-           [chdl.alpha.proto :as proto]))
+           [chdl.alpha.proto :as proto]
+           [chdl.gamma.protocols :as gproto]))
 
-
-;(t/slog-vec 512 1)
-
-(def Ms 
-  (map
-    #(signal (slog-vec 32 (vec-nth message (* 32 %) (* 32 (inc %)))))
-    (range 0 16)))
 
 (defchip chunk-hasher-chip
-         :ports [message  (t/in-sig (t/slog-vec 512))
-                 md5-hash (t/out-sig (t/slog-vec 128))]
-         :internal [M (mapv (fn [_] (t/signal (t/std-uint 32))) (range 16))]
-        (map-indexed
-          #(>! %2 (slog-vec->std-uint (vec-nth message (* 32 %1) (* 32 (inc %1)))))
-          M))
+  :ports [message  (t/in-sig (t/slog-vec 512))
+          md5-hash (t/out-sig (t/slog-vec 128))]
+  :internal [M (mapv (fn [_] (t/signal (t/std-uint 32))) (range 16))]
+
+ (map-indexed
+   #(>! %2 (slog-vec->std-uint
+     (vec-nth message (* 32 %1) (* 32 (inc %1))))) M))
 
 (comment
-  (println (proto/to-str chunk-hasher-chip))
-  )
-
-(macroexpand-1
-  '(chip 
-           :ports [ message  (t/in-sig (t/slog-vec 512))
-                    md5-hash (t/in-sig (t/slog-vec 128))]
-           :internal [M (mapv (fn [_] (t/signal (t/std-uint 32))) (range 16))]))
+  (println (gproto/construct chunk-hasher-chip))
+)
